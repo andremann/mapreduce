1ac07f14d03c9f769001e1c0ec032dbc8f321689
andremann <andremann@libero.it>
2015-07-06 11:32:33 +0200

    corrected text

--- src/test/java/mapreduce/ReadFromHDFSTest.java
+++ src/test/java/mapreduce/ReadFromHDFSTest.java
@@ -15,7 +15,7 @@ public class ReadFromHDFSTest {
 	public void testRead() throws IOException {
 		System.out.println("\n TESTING: " + this.getClass().getCanonicalName());
 		String path = "/params.txt";
-		GaussianParams[] params = GaussianParams.parse(new InputStreamReader(getClass().getResourceAsStream(path)), 2, 3);
+		GaussianParams[] params = GaussianParams.parse(new InputStreamReader(getClass().getResourceAsStream(path)), 2, 2);
 
 		for (int i = 0; i < params.length; i++) {
 			String output = String.format("%s \t %s \t %s", params[i].getWasString(), params[i].getMuAsString(), params[i].getSigmaSqrAsString());
